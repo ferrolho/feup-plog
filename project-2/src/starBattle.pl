57482f2aba371f323fca5a84036d68fd34f67db3
Henrique Ferrolho <henriqueferrolho@gmail.com>
2014-12-04 15:33:46 +0000

    added project-2 folder and base

---
+++ project-2/src/starBattle.pl
@@ -0,0 +1,26 @@
+%=============================================%
+%=           ..:: STAR BATTLE ::..           =%
+%=                                           =%
+%=   Type 'starBattle.' to start the game    =%
+%=                                           =%
+%=============================================%
+%=                                           =%
+%=             ..:: Authors ::..             =%
+%=                                           =%
+%=     Henrique Ferrolho && Joao Pereira     =%
+%=                FEUP - 2014                =%
+%=                                           =%
+%=============================================%
+
+%===============%
+%= @@ includes =%
+%===============%
+:- use_module(library(clpfd)).
+:- use_module(library(random)).
+:- use_module(library(system)).
+
+%====================%
+%= @@ game launcher =%
+%====================%
+starBattle:-
+	write('Star Battle').
